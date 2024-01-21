fbe92c53230e367be37e0d5afbabd1655fd9bed0
Krystian <lis.krystian.10@gmail.com>
2024-01-21 16:13:12 +0100

    refactor: move infra DI to infra Extensions

---
+++ CoinDataApi/Infrastructure/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using CoinDataApi.Core.Interfaces.Clients;
+using CoinDataApi.Infrastructure.Clients;
+
+namespace CoinDataApi.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddHttpClient<ICoinApiClient, CoinApiClient>(client =>
+        {
+            client.BaseAddress = new Uri(configuration.GetValue<string>("CoinApi:Url")!);
+        });
+
+        services.Configure<CoinApiOptions>(configuration.GetRequiredSection("CoinApi"));
+
+        return services;
+    }
+}
--- CoinDataApi/Program.cs
+++ CoinDataApi/Program.cs
@@ -1,17 +1,11 @@
 using CoinDataApi.Core.Interfaces.Clients;
 using CoinDataApi.Infrastructure;
-using CoinDataApi.Infrastructure.Clients;
 using CoinDataApi.Infrastructure.Errors;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<ICoinApiClient, CoinApiClient>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("CoinApi:Url")!);
-});
-
-builder.Services.Configure<CoinApiOptions>(builder.Configuration.GetRequiredSection("CoinApi"));
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services
     .AddErrorHandler();
