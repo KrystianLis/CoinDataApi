5d7db5a95b0d7130f77568eae2980b723edd22ac
Krystian <lis.krystian.10@gmail.com>
2024-01-21 17:29:57 +0100

    fix: improve CoinApiClient and set correct url path

--- CoinDataApi/Infrastructure/Clients/CoinApiClient.cs
+++ CoinDataApi/Infrastructure/Clients/CoinApiClient.cs
@@ -2,6 +2,7 @@
 using CoinDataApi.Core.Interfaces.Clients;
 using CoinDataApi.Core.Models;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace CoinDataApi.Infrastructure.Clients;
 
@@ -19,7 +20,16 @@ public class CoinApiClient : ICoinApiClient
     public async Task<IReadOnlyCollection<OhlcvData>> GetOhlcvFromLastDay(string symbolId,
         bool includeEmptyItems = false, CancellationToken token = default)
     {
-        return (await _httpClient.GetFromJsonAsync<IReadOnlyCollection<OhlcvData>>(
-            $"/v1/ohlcv/{symbolId}/latest?period_id=1DAY&include_empty_items={includeEmptyItems}", token))!;
+        var httpResponseMessage = await _httpClient.GetAsync(
+            $"/v1/ohlcv/{symbolId}/latest?period_id=1HRS&limit=24&include_empty_items={includeEmptyItems}", token);
+
+        var responseMessage = await httpResponseMessage.Content.ReadAsStringAsync(token);
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception(responseMessage);
+        }
+
+        return JsonConvert.DeserializeObject<IReadOnlyCollection<OhlcvData>>(responseMessage)!;
     }
 }
