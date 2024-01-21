455fffdc6a3fa0ac984e12d149b9462bbfe86a87
Krystian <lis.krystian.10@gmail.com>
2024-01-21 18:47:57 +0100

    feat: add VwapCalculator

---
+++ CoinDataApi/Application/Helper/VwapCalculator.cs
@@ -0,0 +1,13 @@
+﻿using CoinDataApi.Application.Services;
+
+namespace CoinDataApi.Application.Helper;
+
+public static class VwapCalculator
+{
+    public static decimal CalculateVwap(IReadOnlyList<AggregatedOhlcvData> data)
+    {
+        var totalValue = data.Sum(ohlcvData => ohlcvData.PriceCloseWeighted * ohlcvData.TotalVolume);
+        var totalVolume = data.Sum(ohlcvData => ohlcvData.TotalVolume);
+        return totalVolume != 0 ? totalValue / totalVolume : 0;
+    }
+}
--- CoinDataApi/Application/Services/Service.cs
+++ CoinDataApi/Application/Services/Service.cs
@@ -1,4 +1,5 @@
-﻿using CoinDataApi.Core.Interfaces.Clients;
+﻿using CoinDataApi.Application.Helper;
+using CoinDataApi.Core.Interfaces.Clients;
 
 namespace CoinDataApi.Application.Services;
 
@@ -16,18 +17,16 @@ public class Service : IService
         var bitstampData = await _client.GetOhlcvFromLastDay("bitstamp_spot_btc_usd", token: token);
         var coinbaseData = await _client.GetOhlcvFromLastDay("coinbase_spot_btc_usd", token: token);
 
-        var combinedData = bitstampData
-            .Concat(coinbaseData);
-
-        var totals = combinedData.Aggregate(
-            new { TotalValue = 0m, TotalVolume = 0m }, 
-            (acc, data) => new 
+        var combinedData = bitstampData.Concat(coinbaseData)
+            .GroupBy(data => data.TimePeriodStart)
+            .Select(group => new AggregatedOhlcvData
             {
-                TotalValue = acc.TotalValue + data.PriceClose * data.VolumeTraded,
-                TotalVolume = acc.TotalVolume + data.VolumeTraded
-            });
-    
-        var vwap = totals.TotalVolume != 0 ? totals.TotalValue / totals.TotalVolume : 0;
+                PeriodStart = group.Key,
+                PriceCloseWeighted = group.Sum(data => data.PriceClose * data.VolumeTraded) / group.Sum(data => data.VolumeTraded),
+                TotalVolume = group.Sum(data => data.VolumeTraded)
+            }).ToList();
+
+        var vwap = VwapCalculator.CalculateVwap(combinedData);
         
         return string.Empty;
     }
